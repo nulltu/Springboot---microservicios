2eaeb54ff47d24c8fd938ae98ac299540ed0d41a
Rusbent Matta <rusbent_matta@itrsa.com.ar>
2021-06-24 19:52:53 -0300

    add methods productController

--- src/main/java/chapter/itr/store/product/controller/ProductController.java
+++ src/main/java/chapter/itr/store/product/controller/ProductController.java
@@ -1,12 +1,12 @@
 package chapter.itr.store.product.controller;
 
+import chapter.itr.store.product.entity.Category;
 import chapter.itr.store.product.entity.Product;
 import chapter.itr.store.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,11 +18,37 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping
-    public ResponseEntity<List<Product>> listProduct(){
+    public ResponseEntity<List<Product>> listProduct(@RequestParam(name = "categoryId", required = false) Long categoryId){
         List<Product> products = productService.listAllProduct();
+        if(categoryId == null){
+            products = productService.listAllProduct();
+            if (products.isEmpty()){
+                return ResponseEntity.noContent().build();
+            }
+        }else{
+            products = productService.findByCategory(Category.builder().id(categoryId).build());
+            if (products.isEmpty()){
+                return ResponseEntity.notFound().build();
+            }
+        }
         if(products.isEmpty()){
             return ResponseEntity.noContent().build();
         }
         return  ResponseEntity.ok(products);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable("id") Long id){
+        Product product = productService.getProduct(id);
+        if (product == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(product);
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product){
+        Product productCreate = productService.createProduct(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productCreate);
     }
 }
